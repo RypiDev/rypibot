844280631322494c2eae4ca998754cf5433fa28b
Walidoux <ma.walidkorchi@icloud.com>
2023-04-05 00:01:40 +0000

    refactor(types): add suport for SlashCommandSubcommandsOnlyBuilder

--- src/types/Command.d.ts
+++ src/types/Command.d.ts
@@ -16,6 +16,7 @@ export type CommandExec = (props: CommandProps) => Awaitable<unknown>
 export type CommandMeta =
   | SlashCommandBuilder
   | Omit<SlashCommandBuilder, 'addSubcommand' | 'addSubcommandGroup'>
+  | SlashCommandSubcommandsOnlyBuilder
 export interface Command {
   meta: CommandMeta
   exec: CommandExec
