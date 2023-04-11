ffd9796c94be2499afc1a1add9ea0b921afa49c9
Walidoux <ma.walidkorchi@icloud.com>
2023-04-11 00:26:56 +0000

    feat(types): add Embed alias interface

---
+++ src/types/Embed.d.ts
@@ -0,0 +1,13 @@
+import type {
+  APIApplicationCommandOptionChoice,
+  AttachmentBuilder,
+  EmbedBuilder
+} from 'discord.js'
+
+export interface EmbedYMLBuilder {
+  choice: APIApplicationCommandOptionChoice<string>
+  embeds: {
+    builder: EmbedBuilder
+    image: { embed: EmbedBuilder; file: AttachmentBuilder }
+  }
+}
